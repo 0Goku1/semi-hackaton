8bc878926442f7f8bd946294e3f606d3e574d51f
0Goku1 <s5408w@gmail.com>
2026-08-17 17:40:27 +0900

    1

--- js/secrets.example.js
+++ js/secrets.example.js
@@ -1,6 +1,4 @@
 const SECRETS = {
   KAKAO_JS_KEY: "KAKAO_JS_KEY_HERE",
   API_BASE_URL: "http://127.0.0.1:8000",
-  // 서버 server/.env 의 KAKAO_REST_KEY 와 맞출 것 (차량 Directions)
-  KAKAO_REST_KEY: "KAKAO_REST_KEY_HERE",
 };
